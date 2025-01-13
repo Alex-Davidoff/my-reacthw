66063209ec206b59dbcbb3d1d5fef506236f8caa
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-01-13 14:23:24 +0200

    L3-3-0

---
+++ src/components/MenuComponent/MenuComponent.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router"
+
+export const MenuComponent = () => {
+    return(
+        <div>
+        <Link to="/users" className="font-semibold">Users</Link>
+        </div>
+    )
+}

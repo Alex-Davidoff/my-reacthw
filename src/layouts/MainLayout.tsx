02668f8dce3627d9d1b8bfe779310cf1c3dc7949
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-01-14 18:01:14 +0200

    L4-3

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router"
+import { MenuComponent } from "../components/Menu/MenuComponent"
 
 export const Mainlayout = () => {
     return <>
+        <MenuComponent/>
         <Outlet/>
     </>
 }

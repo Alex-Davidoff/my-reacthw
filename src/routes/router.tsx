f0b2980673390ec919a500bb2bf45dc6164b1741
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-01-17 12:06:07 +0200

    L6-1

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import { LoginPage } from "../pages/LoginPage";
+import { UserPage } from "../pages/UserPage";
+import { PostsPage } from "../pages/PostsPage";
 
-const routes = createBrowserRouter([
-    {path: '/', element: 'MainLayout', children: [
-        {path: 'login', element: '/LoginPage'},
-        {path: ''}
+export const routes = createBrowserRouter([
+    {path: '/', element: <MainLayout/>, children: [
+        {path: 'login', element: <LoginPage/>},
+        {path: '/auth', children: [
+            {path: '/user', element: <UserPage/>},
+            {path: '/posts', element: <PostsPage/>}
+        ]}
     ]}
 ]);

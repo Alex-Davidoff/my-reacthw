f5b010cd277ecc4d5eb4c247fbb7254e642284a5
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-01-19 21:35:18 +0200

    L6-7

--- src/components/UserComponent/UserComponent.tsx
+++ src/components/UserComponent/UserComponent.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IUser } from "../../models/IUserProps";
+import { getCurrentUser } from "../../services/dummy.api.service";
 
 export const UserComponent = () => {
-    const [currentUser, serCurrentUser] = useState<IUser>();
+    const [currentUser, setCurrentUser] = useState<IUser>();
 
-    
+    useEffect(() => {
+        getCurrentUser().then((response) => setCurrentUser(response));
+    },[]);
 
     return(
-        <>UserComponent</>
+        <div>
+            <p>{currentUser?.id}</p>
+            <h2>{currentUser?.firstName} {currentUser?.lastName}</h2>
+            <h3>{currentUser?.username} {currentUser?.email}</h3>
+            <button className="border border-black rounded w-24">Posts of user</button>
+        </div>
     );
 }
--- src/services/dummy.api.service.ts
+++ src/services/dummy.api.service.ts
@@ -2,8 +2,15 @@ import axios from "axios"
 import { IUserLoginPass, IUserLoginPassExp } from "../models/IUserLoginProps"
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: `${import.meta.env.VITE_API_URL}/auth`
 })
+
+axiosInstance.interceptors.request.use((requestObj) => {
+    if (requestObj.method?.toUpperCase()==='GET'){
+        requestObj.headers.Authorization = 'Bearer ' + localStorage.getItem('aT');
+    }
+    return requestObj;
+});
 
 export const userLogin = async ({username, password}: IUserLoginPass) => {
     const userProps: IUserLoginPassExp = {
@@ -11,6 +18,12 @@ export const userLogin = async ({username, password}: IUserLoginPass) => {
         password: password,
         expiresInMins : import.meta.env.VITE_TOKEN_LIFETIME
         }
-    const loginAnswer =  await axiosInstance.post('/auth/login', userProps);
+    const loginAnswer =  await axiosInstance.post('/login', userProps);
     return loginAnswer;
+}
+
+export const getCurrentUser = async () => {
+    const {data} = await axiosInstance.get('/me');
+    console.log(data);
+    return data;
 }

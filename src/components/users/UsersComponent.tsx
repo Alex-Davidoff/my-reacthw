ecaafeca480dcfc62c9b79307e4e7ff0dac31c0c
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-01-27 13:52:05 +0200

    L7-1-2

--- src/components/users/UsersComponent.tsx
+++ src/components/users/UsersComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UserComponent } from "./UserComponent"
 import { UsersMenuComponent } from "./UsersMenuComponent";
 
@@ -6,6 +6,12 @@ export const UsersComponent = () => {
     console.log('Users');
 
     const [users, setUsers] = useState();
+
+    const [count, setCount] = useState(0);
+
+    const increment = useCallback(() => {
+      setCount(prevCount => prevCount + 1);
+    }, []);
 
     useEffect(() => {
         fetch('https://dummyjson.com/users')
@@ -16,8 +22,13 @@ export const UsersComponent = () => {
 
     return(
         <div className="Users">
+            
             <UsersMenuComponent/>
             <UserComponent/>
+            <p>{count}</p>
+            <button onClick={increment}>
+                Inc
+            </button>
         </div>
     )
 }
